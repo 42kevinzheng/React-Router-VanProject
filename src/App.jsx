c631bafd81c995ad6316653c2e0f826494aa392d
Apple <fullbird@Mac.home>
2025-08-21 19:38:00 -0400

    change images

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import {
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
-  Route,
-  Link
+  Route
 } from "react-router-dom"
 import Home from './pages/Home'
 import About from './pages/About'
@@ -34,10 +33,7 @@ const router = createBrowserRouter(createRoutesFromElements(
           <Route path='/'element={<Layout/>}>
             <Route index element={<Home/>}/>
             <Route path="about" element={<About/>}/>
-                <Route
-      path="login"
-      element={<Login />}
-    />
+            <Route path="login" element={<Login />}/>
             <Route path="vans" element={<Vans/>} loader= {vansLoader }  errorElement={<Error />} />
             <Route path="vans/:id" element={<VanDetails/>}/>
 
@@ -46,6 +42,7 @@ const router = createBrowserRouter(createRoutesFromElements(
               <Route path="income" element={<Income />} />
               <Route path="reviews" element={<Reviews />} />
               <Route path="vans" element={<HostVans />} />
+
               <Route path="vans/:id" element={<HostVanDetail />}>
                 <Route index element={<HostVanInfo />} />
                 <Route path="pricing" element={<HostVanPricing />} />
@@ -55,8 +52,6 @@ const router = createBrowserRouter(createRoutesFromElements(
             <Route path="*" element={<NotFound />}/>
           </Route>
 ))
-
-
 
   return (
      <RouterProvider router={router} />
