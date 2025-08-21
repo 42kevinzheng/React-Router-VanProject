8903e137e3ecc309fcc3b771efe2fc55bb9bc5a7
Apple <fullbird@Mac.home>
2025-08-21 18:03:34 -0400

    add new components and refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import HostVanDetail from './pages/HostVanDetail'
 import HostVanInfo from './Host/HostVanInfo'
 import HostVanPricing from './Host/HostVanPricing'
 import HostVanPhotos from './Host/HostVanPhotos'
+import NotFound from './pages/NotFound'
 
 import "./server"
 
@@ -41,6 +42,7 @@ function App() {
                 <Route path="photos" element={<HostVanPhotos />} />
               </Route>
             </Route>
+            <Route path="*" element={<NotFound />}/>
           </Route>
         </Routes>
       </BrowserRouter>
